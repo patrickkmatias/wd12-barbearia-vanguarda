1d7417cf6efafc9c8b02bbd6c1b5ed03de617071
Patrick M <patrickmatias.silva@hotmail.com>
2023-07-15 10:24:47 -0300

    fim seção hero, substituir pasta images por public

--- assets/sections/hero-section.js
+++ assets/sections/hero-section.js
@@ -1,19 +1,10 @@
 import { LitElement, html, css } from "lit";
+import { section } from "../styles/section-style";
 
 export class HeroSection extends LitElement {
   static styles = [
+    section,
     css`
-      :host {
-        width: 100%;
-        height: 100%;
-
-        display: flex; /* Auto layout */
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 4.375rem;
-      }
-
       section {
         display: flex;
         flex-direction: column;
@@ -42,6 +33,15 @@ export class HeroSection extends LitElement {
         right: -72px;
         top: -39px;
       }
+
+      img {
+        height: 100%;
+      }
+
+      app-paragrafo {
+        font-size: 1.25rem;
+      }
+
     `,
   ];
 
@@ -52,7 +52,9 @@ export class HeroSection extends LitElement {
           <h1>Barbearia <span>Vanguarda</span></h1>
           <app-logo></app-logo>
         </app-titulo>
-        <app-quadro></app-quadro>
+        <app-quadro>
+          <img src="slider-1.jpg" alt="Imagem slider 1" />
+        </app-quadro>
       </section>
       <app-paragrafo>
         Obtenha um estilo impecável, do cabelo à barba.
